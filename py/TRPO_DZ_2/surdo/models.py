ba1ec6604f3c84036d1e36412bdf5dd0b7f90223
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2023-11-15 19:43:00 +0300

    user "table"

--- py/TRPO_DZ_2/surdo/models.py
+++ py/TRPO_DZ_2/surdo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+# Имитация таблицы пользователя (для паттернов)
+class AppUser(models.Model):
+    id_user = models.AutoField(primary_key=True)
+    username = models.CharField(verbose_name='Имя пользователя')
+    password = models.CharField(verbose_name='Пароль')
+    first_name = models.CharField(verbose_name='Имя')
+    middle_name = models.CharField(verbose_name='Отчество')
+    last_name = models.CharField(verbose_name='Фамилия')
