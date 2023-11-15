d3bf95cd5e8fb9b57f92d28e204a9b2c6172cbf5
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2023-11-15 19:52:57 +0300

    Таблицы заданий и ответов

--- py/TRPO_DZ_2/surdo/models.py
+++ py/TRPO_DZ_2/surdo/models.py
@@ -11,3 +11,16 @@ class AppUser(models.Model):
     first_name = models.CharField(verbose_name='Имя')
     middle_name = models.CharField(verbose_name='Отчество')
     last_name = models.CharField(verbose_name='Фамилия')
+
+
+class Task(models.Model):
+    id_task = models.AutoField(primary_key=True)
+    task_title = models.CharField('Название задания')
+    task_text = models.CharField('Текст задания')
+    task_author = models.ForeignKey(AppUser, on_delete=models.CASCADE, verbose_name='Автор задания')
+
+
+class Answer(models.Model):
+    answer_author = models.ForeignKey(AppUser, on_delete=models.CASCADE, verbose_name='Автор ответа')
+    answer_text = models.CharField('Текст ответа')
+    answer_mark = models.IntegerField('Оценка', default=0)
