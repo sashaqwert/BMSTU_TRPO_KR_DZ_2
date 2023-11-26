7409d387e9da4d716bdb46d5f524a1d0bfde5ba7
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2023-11-26 17:34:01 +0300

    user del password

--- py/TRPO_DZ_2/surdo/models.py
+++ py/TRPO_DZ_2/surdo/models.py
@@ -7,7 +7,6 @@ from django.db import models
 class AppUser(models.Model):
     id_user = models.AutoField(primary_key=True)
     username = models.CharField(verbose_name='Имя пользователя')
-    password = models.CharField(verbose_name='Пароль')
     first_name = models.CharField(verbose_name='Имя')
     middle_name = models.CharField(verbose_name='Отчество')
     last_name = models.CharField(verbose_name='Фамилия')
