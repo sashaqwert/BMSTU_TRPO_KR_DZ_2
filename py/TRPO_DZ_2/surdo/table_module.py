e3c36d710a819eb43106bf9fa0674c18a783eea6
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2023-11-29 17:55:24 +0300

    Проверка существования

--- py/TRPO_DZ_2/surdo/table_module.py
+++ py/TRPO_DZ_2/surdo/table_module.py
@@ -3,8 +3,8 @@ from surdo.gateways import AppUserGateway, TaskGateway, AnswerGateway
 
 class AppUserModel:
     @staticmethod
-    def list(id_: int):
-        return AppUserGateway.find_user(id_)
+    def check_exists(id_: int):
+        return AppUserGateway.find_user(id_) is None
 
 
 class TaskModel:
