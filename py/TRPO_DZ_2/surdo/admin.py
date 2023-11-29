493035497412d19af447a7cac8b56d9ce2d3c959
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2023-11-29 16:29:21 +0300

    Возможность управлять фиктивными пользователями через админку

--- py/TRPO_DZ_2/surdo/admin.py
+++ py/TRPO_DZ_2/surdo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from surdo.models import AppUser
+
+
 # Register your models here.
+
+
+@admin.register(AppUser)
+class AppUserAdmin(admin.ModelAdmin):
+    pass
