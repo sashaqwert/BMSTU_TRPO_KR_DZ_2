3adaabc311da4a969f26bdc6c7f14df0d41bc076
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2023-11-29 18:23:22 +0300

    fix models.py

--- py/TRPO_DZ_2/surdo/models.py
+++ py/TRPO_DZ_2/surdo/models.py
@@ -6,22 +6,22 @@ from django.db import models
 # Имитация таблицы пользователя (для паттернов)
 class AppUser(models.Model):
     id_user = models.AutoField(primary_key=True)
-    username = models.CharField(verbose_name='Имя пользователя', default='')
-    first_name = models.CharField(verbose_name='Имя', default='')
-    middle_name = models.CharField(verbose_name='Отчество', default='')
-    last_name = models.CharField(verbose_name='Фамилия', default='')
+    username = models.CharField(verbose_name='Имя пользователя', default='', max_length=255)
+    first_name = models.CharField(verbose_name='Имя', default='', max_length=255)
+    middle_name = models.CharField(verbose_name='Отчество', default='', max_length=255)
+    last_name = models.CharField(verbose_name='Фамилия', default='', max_length=255)
 
 
 class Task(models.Model):
     id_task = models.AutoField(primary_key=True)
-    task_title = models.CharField('Название задания', default='')
-    task_text = models.CharField('Текст задания', default='')
+    task_title = models.CharField('Название задания', default='', max_length=255)
+    task_text = models.CharField('Текст задания', default='', max_length=255)
     task_author = models.ForeignKey(AppUser, on_delete=models.CASCADE, verbose_name='Автор задания', default=1)
 
 
 class Answer(models.Model):
     id_answer = models.AutoField(primary_key=True)
-    answer_author = models.ForeignKey(AppUser, on_delete=models.CASCADE, verbose_name='Автор ответа', default=1)
-    answer_task = models.ForeignKey(Task, on_delete=models.CASCADE, verbose_name='Задание', default='')
-    answer_text = models.CharField('Текст ответа', default='')
+    answer_author = models.ForeignKey(AppUser, on_delete=models.CASCADE, verbose_name='Автор ответа', default=1, max_length=255)
+    answer_task = models.ForeignKey(Task, on_delete=models.CASCADE, verbose_name='Задание', default=1)
+    answer_text = models.CharField('Текст ответа', default='', max_length=255)
     answer_mark = models.IntegerField('Оценка', default=0)
