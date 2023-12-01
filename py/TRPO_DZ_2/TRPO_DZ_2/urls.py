6ab58e673d3069ff2fc6dfb54a9c89c87f88df12
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2023-12-01 10:43:38 +0300

    Форма авторизации (URL)

--- py/TRPO_DZ_2/TRPO_DZ_2/urls.py
+++ py/TRPO_DZ_2/TRPO_DZ_2/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from surdo import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main_page)
 ]
