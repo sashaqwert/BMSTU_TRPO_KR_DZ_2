39530fdf89038b545c413ff4156bfa071c814356
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2023-12-01 11:56:45 +0300

    Редирект при успешном входе

--- py/TRPO_DZ_2/surdo/views.py
+++ py/TRPO_DZ_2/surdo/views.py
@@ -6,11 +6,11 @@ from surdo.table_module import AppUserModule
 
 # Create your views here.
 def main_page(request):
-    if request.method == 'GET':
-        form = forms.LoginForm()
-        return render(request, 'login.html', {'title': 'Авторизация', 'form': form})
-    elif request.method == 'POST':
+    if request.method == 'POST':
         form = forms.LoginForm(request.POST)
         if form.is_valid():
             if AppUserModule.check_exists(form.username):  # Обращаемся к паттерну бизнес-логики
                 return redirect(f'/user/{form.username}/', permanent=False)
+    else:
+        form = forms.LoginForm
+        return render(request, 'login.html', {'title': 'Авторизация', 'form': form})
