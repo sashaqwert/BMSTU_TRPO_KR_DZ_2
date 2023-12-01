288c823b6fe60b4d09931dce717eac698554026b
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2023-12-01 10:41:24 +0300

    Форма авторизации

--- py/TRPO_DZ_2/surdo/views.py
+++ py/TRPO_DZ_2/surdo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from surdo import forms
+
+
 # Create your views here.
+def main_page(request):
+    if request.method == 'GET':
+        form = forms.LoginForm
+        return render(request, 'login.html', {'title': 'Авторизация', 'form': form})
