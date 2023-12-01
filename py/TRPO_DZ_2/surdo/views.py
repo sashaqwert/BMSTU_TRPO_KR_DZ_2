7bb2b8a4db5a13d4f2ac45edd6bb09502d0c5c08
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2023-12-01 13:17:52 +0300

    fix login

--- py/TRPO_DZ_2/surdo/views.py
+++ py/TRPO_DZ_2/surdo/views.py
@@ -9,9 +9,14 @@ from surdo.table_module import AppUserModule
 
 # Create your views here.
 class main_page(APIView):
+    """
+    {"username": "admin"}
+    """
+
     def post(self, request, *args, **kwargs):
         serializer = serializers.loginSerializer(data=request.data)
         if serializer.is_valid():
             if AppUserModule.check_exists(serializer.data['username']):  # Обращаемся к паттерну бизнес-логики
                 return redirect(f'/user/{serializer.data["username"]}/', permanent=False)
         return Response('Несуществующий пользователь', status=status.HTTP_404_NOT_FOUND)
+
