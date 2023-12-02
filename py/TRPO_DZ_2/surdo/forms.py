8b8f57b57636b0999c78c090a6e6bf9b9091aaab
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2023-12-02 20:17:36 +0300

    Форма добавления задания

--- py/TRPO_DZ_2/surdo/forms.py
+++ py/TRPO_DZ_2/surdo/forms.py
@@ -3,3 +3,9 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='username', max_length=100)
+
+
+class TaskAddForm(forms.Form):
+    author_id = forms.IntegerField(label='ID автора')
+    title = forms.CharField(max_length=100, label='Название задания')
+    text = forms.CharField(widget=forms.Textarea, label='Текст задания')
