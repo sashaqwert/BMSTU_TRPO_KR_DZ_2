a927be9fe3e5a0419a6589899459a2961c7eae5f
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2023-12-06 11:10:05 +0300

    Форма добавления ответа

--- py/TRPO_DZ_2/surdo/forms.py
+++ py/TRPO_DZ_2/surdo/forms.py
@@ -9,3 +9,9 @@ class TaskAddForm(forms.Form):
     author_id = forms.IntegerField(label='ID автора')
     title = forms.CharField(max_length=100, label='Название задания')
     text = forms.CharField(widget=forms.Textarea, label='Текст задания')
+
+
+class AnswerAddForm(forms.Form):
+    author_id = forms.IntegerField(label='ID автора')
+    task_id = forms.IntegerField(label='ID задания')
+    text = forms.CharField(widget=forms.Textarea, label='Текст ответа')
