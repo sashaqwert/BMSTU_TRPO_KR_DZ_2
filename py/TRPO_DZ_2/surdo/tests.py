774efbfc1b117f502d64887ed7ae8867fae377d5
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2023-12-24 18:55:41 +0300

    tests.py + database

--- py/TRPO_DZ_2/surdo/tests.py
+++ py/TRPO_DZ_2/surdo/tests.py
@@ -3,5 +3,17 @@
 """
 
 from django.test import TestCase
+import surdo.models as models
+import surdo.gateways as gateways
+import surdo.table_module as table_module
 
 # Create your tests here.
+class TestUser(TestCase):
+    def setUp(self):
+        pass  # Пустая БД
+
+    def test_create(self):
+        table_module.AppUserModule.insert(1, 'admin', 'Александр', 'Евгеньевич', 'Чиварзин')
+        obj = models.AppUser.objects.get(id=1)
+        self.assertEqual(obj.username, 'admin')
+        self.assertEqual(obj.id_user, 1)
