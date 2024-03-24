d822e8daeab3bf8fd8d83a2df045e0736bbb4d30
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2024-03-24 17:35:37 +0300

    Абстрактная реализация паттерна "Наблюдатель"

---
+++ py/TRPO_DZ_2/surdo/Observer.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class Observer(ABC):
+    """
+    Интерфейс Наблюдателя объявляет метод уведомления, который издатели
+    используют для оповещения своих подписчиков.
+    """
+
+    @abstractmethod
+    def update(self, subject: Subject) -> None:
+        """
+        Получить обновление от субъекта.
+        """
+        pass
+
+
+"""
+Конкретные Наблюдатели реагируют на обновления, выпущенные Издателем, к которому
+они прикреплены.
+"""
+
+
+class Subject(ABC):
+    """
+    Интерфейс издателя объявляет набор методов для управления подписчиками.
+    """
+
+    @abstractmethod
+    def attach(self, observer: Observer) -> None:
+        """
+        Присоединяет наблюдателя к издателю.
+        """
+        pass
+
+    @abstractmethod
+    def detach(self, observer: Observer) -> None:
+        """
+        Отсоединяет наблюдателя от издателя.
+        """
+        pass
+
+    @abstractmethod
+    def notify(self) -> None:
+        """
+        Уведомляет всех наблюдателей о событии.
+        """
+        pass
