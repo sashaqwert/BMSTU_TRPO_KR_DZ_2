de34209f474bf7b68eee25b1e1b01bdcb5686602
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2024-03-24 19:48:43 +0300

    Наблюдатель... Исправлен запуск на Windows без Docker

--- py/TRPO_DZ_2/AnswerMonitor.py
+++ py/TRPO_DZ_2/AnswerMonitor.py
@@ -2,18 +2,20 @@
 Скрипт для демонстрации паттерна "Наблюдатель".
 Команда для запуска: python manage.py shell < AnswerMonitor.py
 """
-from surdo.Observer import Observer, Subject
-from surdo.gateways import AnswerGateway
-from surdo.models import Answer
+from surdo.Observer import Subject, Observer
 
 
 class AnswerObserver(Observer):
     def update(self, subject: Subject) -> None:
+        from surdo.models import Answer
+        from surdo.models import Answer
         count = Answer.objects.filter(answer_mark__isnull=True).count()
         print(f'Непроверенных ответов: {count}')
 
 
 def main():
+    from surdo.gateways import AnswerGateway
+
     ob = AnswerObserver()
     g = AnswerGateway(-1, 1, 1, "", 1)
     g.attach(ob)
@@ -26,6 +28,15 @@ def main():
     pass
 
 
+def prepare():
+    import os
+    import django
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TRPO_DZ_2.settings")
+    django.setup()
+
+
+if __name__ == '__main__':
+    prepare()
 if (__name__ == '__main__') or (__name__ == 'django.core.management.commands.shell'):
     main()
 else:
